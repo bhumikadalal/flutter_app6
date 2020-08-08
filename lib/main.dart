260dbeb2f98af90f1501bb7c82e3801c7cd5727a
Bhumika <bhumikadalal949@gmail.com>
2020-08-08 20:21:46 +0530

    added my name as text

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(
+              'Bhumika Rajendra Dalal'
+            ),
           ],
         ),
       ),
